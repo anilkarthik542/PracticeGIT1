e8ba4af3780a629bcdf9241412c306911362d63d
anil karthik <anil_karthik@epam.com>
2026-02-19 23:16:23 +0530

    modified in import stmt

--- tests/poms/test_runner/test_login.spec.js
+++ tests/poms/test_runner/test_login.spec.js
@@ -1,5 +1,5 @@
-import { test as base, expect } from '@playwright/test'
-import { LoginPage } from '../pages/login.spec'
+import { test as base, expect } from '@playwright/test';
+import { LoginPage } from '../pages/login.spec';
 
 const test = base.extend({
     login: async ({ page }, use) => {
